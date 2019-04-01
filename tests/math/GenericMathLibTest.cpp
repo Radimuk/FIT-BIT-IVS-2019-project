b98dec7c53919777797c8e9acb8c4fb11543c3a0
Pavel <45284814+xraurp@users.noreply.github.com>
2019-04-01 15:10:28 +0200

    Math lib: add tests (#24)
    * Tests for all math libs
    * fixed wrong argument in root test
    * fixed trigonometric tests, added testing for exceptions (division by zero etc.)

--- tests/math/GenericMathLibTest.cpp
+++ tests/math/GenericMathLibTest.cpp
@@ -31,6 +31,74 @@ namespace FitCalcTests {
 
 	TEST_F(GenericMathLibTest, add) {
 		ASSERT_EQ(5, this->mathLib.add(2, 3));
+		ASSERT_EQ(20, this->mathLib.add(0, 20));
+		ASSERT_EQ(-1, this->mathLib.add(2, -3));
 	}
+
+	TEST_F(GenericMathLibTest, sub) {
+		ASSERT_EQ(-1, this->mathLib.sub(2, 3));
+		ASSERT_EQ(4, this->mathLib.sub(7, 3));
+		ASSERT_EQ(1, this->mathLib.sub(-2, -3));
+	}
+
+	TEST_F(GenericMathLibTest, mul) {
+		ASSERT_EQ(10, this->mathLib.mul(2, 5));
+		ASSERT_EQ(-10, this->mathLib.mul(-2, 5));
+		ASSERT_EQ(0, this->mathLib.mul(2, 0));
+	}
+
+	TEST_F(GenericMathLibTest, div) {
+		ASSERT_EQ(4, this->mathLib.div(12, 3));
+		ASSERT_EQ(0.6, this->mathLib.div(12, 20));
+		ASSERT_EQ(-24, this->mathLib.div(-12, 0.5));
+		ASSERT_ANY_THROW(this->mathLib.div(10, 0));
+	}
+
+	TEST_F(GenericMathLibTest, mod) {
+		ASSERT_EQ(0, this->mathLib.mod(12, 3));
+		ASSERT_EQ(1, this->mathLib.mod(13, 3));
+		ASSERT_EQ(5, this->mathLib.mod(15, 10));
+		ASSERT_ANY_THROW(this->mathLib.mod(10, 0));
+	}
+
+	TEST_F(GenericMathLibTest, abs) {
+		ASSERT_EQ(2, this->mathLib.abs(2));
+		ASSERT_EQ(4.5, this->mathLib.abs(-4.5));
+		ASSERT_EQ(0, this->mathLib.abs(0));
+	}
+
+	TEST_F(GenericMathLibTest, fact) {
+		ASSERT_EQ(3628800, this->mathLib.fact(10));
+		ASSERT_EQ(6, this->mathLib.fact(3));
+		ASSERT_EQ(1, this->mathLib.fact(0));
+	}
+
+	TEST_F(GenericMathLibTest, pow) {
+		ASSERT_EQ(8, this->mathLib.pow(2, 3));
+		ASSERT_EQ(7776, this->mathLib.pow(6, 5));
+		ASSERT_EQ(4, this->mathLib.pow(2, 2));
+	}
+
+	TEST_F(GenericMathLibTest, root) {
+		ASSERT_EQ(3, this->mathLib.root(9, 2));
+		ASSERT_EQ(1.414213562, this->mathLib.root(2, 2));
+		ASSERT_EQ(3.107232506, this->mathLib.root(30, 3));
+	}
+
+	TEST_F(GenericMathLibTest, log) {
+		ASSERT_EQ(2, this->mathLib.log(100));
+		ASSERT_EQ(1.079181246, this->mathLib.log(12));
+		ASSERT_EQ(-0.301029996, this->mathLib.log(0.5));
+		ASSERT_ANY_THROW(this->mathLib.log(-2));
+	}
+
+	TEST_F(GenericMathLibTest, ln) {
+		ASSERT_EQ(2.302585093, this->mathLib.ln(10));
+		ASSERT_EQ(0.693147181, this->mathLib.ln(2));
+		ASSERT_EQ(-0.693147181, this->mathLib.ln(0.5));
+		ASSERT_ANY_THROW(this->mathLib.ln(-2));
+	}
+
+
 
 }
--- tests/math/TrigonometricMathLibTest.cpp
+++ tests/math/TrigonometricMathLibTest.cpp
@@ -29,8 +29,21 @@ namespace FitCalcTests {
 		TrigonometricMathLib mathLib;
 	};
 
-	TEST_F(TrigonometricMathLibTest, add) {
+	TEST_F(TrigonometricMathLibTest, sin) {
 		ASSERT_EQ(0, this->mathLib.sin(0));
+		ASSERT_EQ(1, this->mathLib.sin(M_PI/2));
+		ASSERT_EQ(0.707106781, this->mathLib.sin(M_PI/4));
 	}
 
+	TEST_F(TrigonometricMathLibTest, cos) {
+		ASSERT_EQ(0, this->mathLib.cos(M_PI/2));
+		ASSERT_EQ(-1, this->mathLib.cos(M_PI));
+		ASSERT_EQ(0.707106781, this->mathLib.cos(M_PI/4));
+	}
+
+	TEST_F(TrigonometricMathLibTest, tan) {
+		ASSERT_EQ(0, this->mathLib.tan(0));
+		ASSERT_EQ(1, this->mathLib.tan(M_PI/4));
+		ASSERT_ANY_THROW(this->mathLib.tan(3*M_PI/2));
+	}
 }
