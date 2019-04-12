f1533fae3f9c580577f77a7a4228147f47355aba
Radim Lipka <45268218+Radimuk@users.noreply.github.com>
2019-04-12 21:54:43 +0200

    Grammar: add basic tests (#28)
    * Grammar: add basic tests
    * Grammar: fix visibility of error property in tests
    
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- tests/grammar/calculatorBaseVisitorTest.cpp
+++ tests/grammar/calculatorBaseVisitorTest.cpp
@@ -28,6 +28,8 @@
 namespace FitCalcTests {
 
 	class calculatorBaseVisitorTest : public ::testing::Test {
+	protected:
+		double error = 1e-6;
 	};
 
 	TEST_F(calculatorBaseVisitorTest, add) {
@@ -54,4 +56,184 @@ namespace FitCalcTests {
 		EXPECT_EQ(expected, actual);
 	}
 
+	TEST_F(calculatorBaseVisitorTest, mul) {
+		antlr4::ANTLRInputStream input("1.0 * 2");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 2.0;
+		EXPECT_EQ(expected, actual);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, div) {
+		antlr4::ANTLRInputStream input("2.0 / 2");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 1.0;
+		EXPECT_EQ(expected, actual);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, mod) {
+		antlr4::ANTLRInputStream input("9.0 mod 2");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 1.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, abs) {
+		antlr4::ANTLRInputStream input("|-9.0|");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 9.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, fact) {
+		antlr4::ANTLRInputStream input("5.0!");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 120.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, pow) {
+		antlr4::ANTLRInputStream input("2.0^3");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 8.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, log) {
+		antlr4::ANTLRInputStream input("log 100.0");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 2.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, ln) {
+		antlr4::ANTLRInputStream input("ln 10.0");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = M_LN10;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+
+	TEST_F(calculatorBaseVisitorTest, sin) {
+		antlr4::ANTLRInputStream input("sin 0");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 0.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, cos) {
+		antlr4::ANTLRInputStream input("cos 0");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 1.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, tan) {
+		antlr4::ANTLRInputStream input("tan 0");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 0.0;
+		EXPECT_NEAR(expected, actual, this->error);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, addPercentage) {
+		antlr4::ANTLRInputStream input("10.0 + 20%");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 12.0;
+		EXPECT_EQ(expected, actual);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, subPercentage) {
+		antlr4::ANTLRInputStream input("10.0 - 20%");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 8.0;
+		EXPECT_EQ(expected, actual);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, mulPercentage) {
+		antlr4::ANTLRInputStream input("18.0 * 10%");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 1.8;
+		EXPECT_EQ(expected, actual);
+	}
+
+	TEST_F(calculatorBaseVisitorTest, divPercentage) {
+		antlr4::ANTLRInputStream input("18.0 / 20%");
+		calculatorLexer lexer(&input);
+		antlr4::CommonTokenStream tokens(&lexer);
+		calculatorParser parser(&tokens);
+		calculatorParser::ExpressionContext* expresion = parser.expression();
+		calculatorBaseVisitor visitor;
+		double actual = visitor.visitExpression(expresion);
+		double expected = 90.0;
+		EXPECT_EQ(expected, actual);
+	}
 }
