5265fd41f37ba449de8923bb96b37266d4fd68e8
Roman Ondráček <ondracek.roman@centrum.cz>
2019-04-20 16:10:56 +0200

    Profiling: use std::vector instead of an static array, use std::cout and std::cerr instead of std::(f)printf
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- src/profiling/profiling.cpp
+++ src/profiling/profiling.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "../math/GenericMathLib.cpp"
 
 /**
@@ -7,28 +8,28 @@
  */
 int main() {
 	GenericMathLib genericMath;
-	double array[1024];
-	unsigned int enteredNumbers = 0;
-	while (std::scanf("%lf", &array[enteredNumbers]) == 1) {
-		enteredNumbers++;
+	std::vector<double> array;
+	double item;
+	while (std::scanf("%lf", &item) == 1) {
+		array.push_back(item);
 	}
-	if (enteredNumbers == 0 || enteredNumbers == 1){
-		fprintf(stderr, "Unable to count the deviation out of zero or one number.\n");
+	if (array.size() <= 1) {
+		std::cerr << "Unable to count the deviation out of zero or one number." << std::endl;
 		return 1;
 	}
 	double sumOfSquares = 0;
 	double average = 0;
-	for (unsigned int i = 0; i < enteredNumbers; i++) {
-		double elementSquared = genericMath.pow(array[i], 2.0);
+	for (double const &element: array) {
+		double elementSquared = genericMath.pow(element, 2.0);
 		sumOfSquares = genericMath.add(sumOfSquares, elementSquared);
-		average = genericMath.add(average, array[i]);
+		average = genericMath.add(average, element);
 	}
-	average = genericMath.mul(average, genericMath.div(1.0, enteredNumbers));
+	average = genericMath.mul(average, genericMath.div(1.0, array.size()));
 	average = genericMath.pow(average, 2.0);
-	average = genericMath.mul(average, enteredNumbers);
+	average = genericMath.mul(average, array.size());
 	double result = genericMath.sub(sumOfSquares, average);
-	result = genericMath.div(result, genericMath.sub(enteredNumbers, 1.0));
-	result = genericMath.pow(result, 0.5);
-	std::printf("%f\n", result);
+	result = genericMath.div(result, genericMath.sub(array.size(), 1.0));
+	result = genericMath.root(2.0, result);
+	std::cout << result << std::endl;
 	return 0;
 }
